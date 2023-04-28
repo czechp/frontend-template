6e5158cb6480bd84a631577fbfe1302c26f40cf4
czechp <webcoderc@gmail.com>
2023-04-28 20:48:22 +0200

    Created wildcards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomePageComponent} from "./home/home-page/home-page.component";
+import {PageNotFoundComponent} from "./wildcard/page-not-found/page-not-found.component";
+import {LoginRequiredComponent} from "./wildcard/login-required/login-required.component";
+import {LoginPageComponent} from "./login/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "login", component: LoginPageComponent},
+  {path: "login-required", component: LoginRequiredComponent},
+  {path: "not-found", component: PageNotFoundComponent},
+  {path: "**", redirectTo: "/not-found"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { ContentComponent } from './layout/content/content.component';
+import { LoginRequiredComponent } from './wildcard/login-required/login-required.component';
+import { PageNotFoundComponent } from './wildcard/page-not-found/page-not-found.component';
+import { PageComponent } from './component/page/page.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    HomePageComponent,
+    ContentComponent,
+    LoginRequiredComponent,
+    PageNotFoundComponent,
+    PageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
